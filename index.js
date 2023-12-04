3e9d3766e763b8cd6291bebbfad6be8fc6bb75f5
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-12-04 13:10:46 -0500

    Added more routes and nodemon

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+
+// nodemon index.js
+// nodemon allows server to refresh without having to CTRL C everytime to test new changes
+// npm i -g nodemon -> -g made it global to use on any other project not just this one
+
+// when load "localhost3000" will respond with what's below
+app.get("/", (req, res) => {
+
+    // rawHeaders gives key:value pairs in console log when refresh localhost3000
+    // console.log(req.rawHeaders);
+
+     // res.send("Hello World!");
+
+     res.send("<h1>Hello World!</h1>");
+
+})
+
+app.get("/about", (req, res) => {
+
+    res.send("<h1>About Me </h1>");
+
+})
+
+
 // Method on app that puts on port is location on our server where we listen to requests from client side
 // callback function triggered when our server is setup on port 3000
 app.listen(port, () => {
